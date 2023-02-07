78df65b1899698b0210c34ade1397df61c0f1ec3
mkdemon5 <alvaradomike5@gmail.com>
2023-02-06 22:09:04 -0600

    starting over

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-// import Navbar from './components/Navbar';
-// import Home from './components/Home';
-// import Projects from './components/Projects';
-// import Resume from './components/Resume';
-// import Contact from './components/Contact';
-// import Footer from './components/Footer';
+import Navbar from '../Module 20/React-Portfolio/src/components/Navbar';
+import Home from '../Module 20/React-Portfolio/src/components/Home';
+import Projects from '../Module 20/React-Portfolio/src/components/Projects';
+import Resume from '../Module 20/React-Portfolio/src/components/Resume';
+import Contact from '../Module 20/React-Portfolio/src/components/Contact';
+import Footer from '../Module 20/React-Portfolio/src/components/Footer';
 import { useState } from 'react';
-// import {
-// 	BrowserRouter as Router,
-// 	Route,
-// 	Routes,
-// 	Navigate,
-// } from 'react-router-dom';
-import { VechaiProvider, Button } from '@vechaiui/react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from 'react-router-dom';
 
 import './App.css';
 
 const App = () => {
-	// const [selectedPage, setSelectedPage] = useState('home');
+	const [selectedPage, setSelectedPage] = useState('home');
 
 	return (
-		// 	<Router>
-		// 		<div className=''>
-		// 			<Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-		// 			<div className=''>
-		// 				<Routes>
-		// 					<Route path='/' element={<Home />} />
-		// 					<Route path='/projects' element={<Projects />} />
-		// 					<Route path='/resume' element={<Resume />} />
-		// 					<Route path='/contact' element={<Contact />} />
-		// 					<Route path='*' element={<Navigate to='/' />} />
-		// 				</Routes>
-		// 				<Footer />
-		// 			</div>
-		// 		</div>
-		// 	</Router>
-
-		<VechaiProvider>
-			<Button>Hello Vechai</Button>
-		</VechaiProvider>
+		<Router>
+			<div className=''>
+				<Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+				<div className=''>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/projects' element={<Projects />} />
+						<Route path='/resume' element={<Resume />} />
+						<Route path='/contact' element={<Contact />} />
+						<Route path='*' element={<Navigate to='/' />} />
+					</Routes>
+					<Footer />
+				</div>
+			</div>
+		</Router>
 	);
 };
 
