838b9e6a7fbf3868f30f0eb12f7173eebae440c4
mkdemon5 <alvaradomike5@gmail.com>
2023-02-06 23:53:22 -0600

    built out home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,20 @@
+import newProfile from '../assets/images/newProfilePic.jpeg';
+
 const Home = () => {
     return (
-        <div>
-
-        </div>
+        <section id="home" className="xs:mx-auto md:flex md:justify-between md:items-center md:h-full gap-16 py-5 mt-6">
+            <div className="basis-2/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
+                <img alt="profile" className="w-full max-w-[350px] md:max-w-[600px]" src={newProfile} />
+            </div>
+            <div className='z-30 basis-2/5 mt-12 md:mt-32'>
+                <h1 className='text-6x1 font-itim font-semibold z-10 text-center'>
+                    Michael Alvarado
+                </h1>
+                <p className='mt-10 mb-7 tex-xl text-center'>
+                    Hi, I'm Michael Alvarado. I recently completed my Full Stack Web Development certification from The University of Texas at Austin. 
+                </p>
+            </div>
+        </section>
     );
 };
 
