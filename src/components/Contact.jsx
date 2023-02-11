bf8db526cbcd465e0d4fe1cc86569cec5eaa6451
mkdemon5 <alvaradomike5@gmail.com>
2023-02-11 14:07:05 -0600

    form built, working on submit functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,48 +1,81 @@
+import { useState } from "react";
+import { send } from 'emailjs-com';
+
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        send(
+            'SERVICE ID',
+            'TEMPLATE ID',
+            formData,
+            'User ID'
+        ).then((r) => {
+            console.log('SUCCESS!', r.status, r.text);
+        }).catch((err) => {
+            console.log('FAILED...', err);
+        });
+    };
+
     return (
-        <section>
-            <p>Coming Soon!</p>
+        <section id="contact" className="py-48">
+            <h2 className="font-itim text-center text-emerald font-semibold text-4xl">Get in touch!</h2>
+            <div className="flex md:justify-center my-5 p-2 text-night">
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        className="w-full font-semibold p-3 m-2" 
+                        type='text' 
+                        id="name" 
+                        name="name" 
+                        placeholder='Name' 
+                        onChange={handleChange} 
+                        value={formData.name} 
+                        required 
+                    />
+
+                    <input 
+                        className="w-full font-semibold p-3 m-2" 
+                        type='email' 
+                        id="email" 
+                        name="email" 
+                        placeholder='Email' 
+                        onChange={handleChange} 
+                        value={formData.email} 
+                        required 
+                    />
+
+                    <textarea 
+                        className="w-full font-semibold p-3 m-2" 
+                        id="message" 
+                        name="message" 
+                        placeholder='Message' 
+                        onChange={handleChange} 
+                        value={formData.message} 
+                        required 
+                    />
+
+                    <button 
+                        className='rounded-sm mt-4 ml-2 py-3 px-7 text-lg font-semibold bg-picton-blue text-charcoal hover:bg-light-rose transition duration-500' 
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
         </section>
     );
 };
 
 export default Contact;
-
-// import React, { useState } from 'react';
-
-// function ContactForm() {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     email: '',
-//     message: ''
-//   });
-
-//   const handleChange = (event) => {
-//     setFormData({
-//       ...formData,
-//       [event.target.name]: event.target.value
-//     });
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     // send formData to your email here
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <label htmlFor="name">Name:</label>
-//       <input type="text" id="name" name="name" onChange={handleChange} value={formData.name} />
-
-//       <label htmlFor="email">Email:</label>
-//       <input type="email" id="email" name="email" onChange={handleChange} value={formData.email} />
-
-//       <label htmlFor="message">Message:</label>
-//       <textarea id="message" name="message" onChange={handleChange} value={formData.message} />
-
-//       <button type="submit">Submit</button>
-//     </form>
-//   );
-// }
-
-// export default ContactForm;
